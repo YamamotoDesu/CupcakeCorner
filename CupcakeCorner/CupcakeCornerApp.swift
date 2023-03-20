0d235c5dee6afc3fde099b92d7c993e4426baea0
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-20 20:53:57 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 山本響 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
